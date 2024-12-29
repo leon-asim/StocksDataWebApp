0f1ab52d367baae3c8ee9bbbc73e2e918a4c3bac
leon-asim <leon.asim38@gmail.com>
2024-12-29 22:13:15 +0100

    Moving Averages and Oscillators in Spring commit

--- src/main/java/finki/das/StocksDataWebApp/repository/MovingAverageRepository.java
+++ src/main/java/finki/das/StocksDataWebApp/repository/MovingAverageRepository.java
@@ -3,9 +3,11 @@ package finki.das.StocksDataWebApp.repository;
 import finki.das.StocksDataWebApp.model.MovingAverage;
 import finki.das.StocksDataWebApp.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MovingAverageRepository extends JpaRepository<MovingAverage, Long> {
 
     List<MovingAverage> findAllBySymbolAndType(String symbol, String type);
